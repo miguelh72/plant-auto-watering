deb8a4597f734019e9fac71f06c2619a61402286
Miguel Hernandez <miguelh72@outlook.com>
2022-01-01 17:59:02 -0500

    Hello world express app
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello World! 2');
+exports.__esModule = true;
+var express = require("express");
+var PORT = (process.env.PORT && parseInt(process.env.PORT)) || 3000;
+var HOST = process.env.HOST || 'localhost';
+var app = express();
+app.get('*', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(PORT, HOST, function () { return console.log("Server listening at http://".concat(HOST, ":").concat(PORT)); });
+exports["default"] = app;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World! 2');
+import * as express from 'express';
+
+const PORT: number = (process.env.PORT && parseInt(process.env.PORT)) || 3000;
+const HOST: string = process.env.HOST || 'localhost';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, HOST, () => console.log(`Server listening at http://${HOST}:${PORT}`));
+
+export default app;
